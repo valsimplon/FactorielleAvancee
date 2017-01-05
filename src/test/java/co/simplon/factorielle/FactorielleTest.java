71fffdd55676a463cf0b3872ee0b5cd75447c87e
Eric Siber <eric.siber@gmail.com>
2017-01-05 11:17:23 +0100

    Mise à jour .gitignore + Etape 2

---
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -0,0 +1,14 @@
+package co.simplon.factorielle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FactorielleTest {
+
+	@Test
+	public void testCalculer() {
+		fail("Not yet implemented");
+	}
+
+}
