de918d7cc036dfef8e7124842d49987ebcea8020
Eric Siber <eric.siber@gmail.com>
2017-01-05 12:27:59 +0100

    Ajout d'un cas de test pour la valeur 1

--- src/test/java/co/simplon/factorielle/FactorielleTest.java
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -7,8 +7,17 @@ import org.junit.Test;
 public class FactorielleTest {
 
 	@Test
-	public void testCalculer() {
-		fail("Not yet implemented");
+	public void factorielle_de_1_doit_renvoyer_1() {
+		// GIVEN
+		long entier = 1;
+		long resultatAttendu = 1;
+		Factorielle f = new Factorielle();
+		
+		// WHEN
+		long resultat = f.calculer(entier);
+		
+		// THEN
+		assertEquals(resultatAttendu, resultat);
 	}
 
 }
