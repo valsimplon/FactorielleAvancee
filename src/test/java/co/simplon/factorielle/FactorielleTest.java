585b8d46757911d4ca8476145ff9c37f0eb152a9
Eric Siber <eric.siber@gmail.com>
2017-01-05 16:55:26 +0100

    Ajout de tests et détection d'un bug pour l'entier 0

--- src/test/java/co/simplon/factorielle/FactorielleTest.java
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -19,5 +19,31 @@ public class FactorielleTest {
 		// THEN
 		assertEquals(resultatAttendu, resultat);
 	}
+	
+	@Test
+	public void factorielle_de_2_doit_renvoyer_2() {
+		Factorielle f = new Factorielle();
+		
+		long resultat = f.calculer(2);
+		
+		assertEquals(2, resultat);
+	}
+	
+	@Test
+	public void factorielle_de_3_doit_renvoyer_6() {
+		Factorielle f = new Factorielle();
+		
+		assertEquals(6, f.calculer(3));
+	}
+	
+	@Test
+	public void factorielle_de_18_doit_renvoyer_6402373705728000() {
+		assertEquals(6402373705728000l, new Factorielle().calculer(18));
+	}
+	
+	@Test
+	public void factorielle_de_0_doit_renvoyer_1() {
+		assertEquals(1, new Factorielle().calculer(0));
+	}
 
 }
