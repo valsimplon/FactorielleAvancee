9f36cd46418a2ce97baf6afea950c094e76c6b51
Eric Siber <eric.siber@gmail.com>
2017-01-09 17:37:10 +0100

    Après configuration des favoris pour les imports statiques dans Eclipse

--- src/test/java/co/simplon/factorielle/FactorielleTest.java
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -5,10 +5,13 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class FactorielleTest {
 
@@ -58,6 +61,7 @@ public class FactorielleTest {
 		// si le test ne passe pas, je n'ai pas de message clair m'indiquant ce
 		// qui était attendu et ce qui a été évalué
 		assertTrue(factorielle.calculer(0) == 1);
+		assertFalse(factorielle.calculer(0) != 1);
 	}
 
 	@Ignore("test incomplet")
@@ -88,6 +92,7 @@ public class FactorielleTest {
 
 		// THEN
 		assertThat(resultat, equalTo(resultatAttendu));
+		assertThat(resultat == resultatAttendu, is(true));
 	}
 
 }
