2ba3c0e6f681b7878017631f6f8ce51383f6bde0
valsimplon <val.simplon@gmail.com>
2017-01-11 14:01:52 +0100

    nouvelle classe

---
+++ src/main/java/co/simplon/factorielle/FactorielleUnvailableException.java
@@ -0,0 +1,13 @@
+package co.simplon.factorielle;
+
+public class FactorielleUnvailableException extends Exception {
+
+	public FactorielleUnvailableException() {
+		super();
+	}
+		
+	public FactorielleUnvailableException(String messageErreur) {
+		super(messageErreur);
+	}
+
+}
